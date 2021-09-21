39601ea8dffe8d3916fa5f1f3254154d3e474c94
Javi Garcia Cespedes <javi.garcia.cespedes@gmail.com>
2021-09-21 10:21:45 +0200

    Initial Commit

---
+++ Shared/Quiz_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Javier García Céspedes on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
